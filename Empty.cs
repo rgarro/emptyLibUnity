bd343dcb065a1ea334a796b8ab21220970a87f79
rgarro <rgarro@gmail.com>
2018-08-23 15:31:33 -0600

    project init

---
+++ Empty.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace emptyLibUnity{
+    public class Empty : MonoBehaviour{
+        // Use this for initialization
+	void Start () {
+		Debug.Log("empty as empty is ...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    }
+}
