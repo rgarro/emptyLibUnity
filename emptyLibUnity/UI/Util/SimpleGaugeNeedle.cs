11ea430ab3a8375f75eb28ce74408a2d409649d5
rgarro <rgarro@gmail.com>
2018-09-04 21:28:54 -0600

    simple Gauge needle will be a rule of three tilt rotation from gauge bg ...

--- emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
+++ emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SimpleGaugeNeedle : MonoBehaviour {
+/**
+ * Tilts Gauge Needle from pivot adjusting tilter value from needle far side z rotations value
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 
-	// Use this for initialization
+namespace emptyLibUnity.UI.Util {
+	public class SimpleGaugeNeedle : MonoBehaviour {
+
+	public Image Needle;
+	public float farLeft = 15.2F;
+	public float farRight = -13.02F;
+
+	protected float tilter;
+	private bool tilterIsSet = false; 
+
 	void Start () {
 		
 	}
 	
+	public void getTilter(float tiltVal){
+		this.tilter = tiltVal;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
 }
