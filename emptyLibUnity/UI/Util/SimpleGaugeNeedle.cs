697b0ed7ecd36803eec3cbfa451bee6cebb9e9cb
rgarro <rgarro@gmail.com>
2018-09-05 19:39:06 -0600

    convert double to float is tomorrow business ....

--- emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
+++ emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
@@ -16,20 +16,29 @@ namespace emptyLibUnity.UI.Util {
 	public float farLeft = 15.2F;
 	public float farRight = -13.02F;
 
-	protected float tilter;
+	protected double tilter;
+	protected double rotation;
+	private float rotationZ = 15.2F;
 	private bool tilterIsSet = false; 
 
 	void Start () {
 		
 	}
 	
-	public void getTilter(float tiltVal){
+	public void getTilter(double tiltVal){
 		this.tilter = tiltVal;
+		Debug.Log("tilt ...");
+		Debug.Log(this.tilter);
+		this.setRotation()
+	}
+
+	protected void setRotation(){
+		this.rotationZ = (this.tilter/this.farLeft)*this.farRight;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		this.Needle.rectTransform.eulerAngles = new Vector3(0,0,rotationZ);
 	}
 }
 }
