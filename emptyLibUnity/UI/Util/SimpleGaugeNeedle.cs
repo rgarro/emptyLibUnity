62541f387cf997526f63fee6d35de4bfc3b06441
rgarro <rgarro@gmail.com>
2018-09-11 14:40:52 -0600

    implementing speedometer styles

--- emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
+++ emptyLibUnity/UI/Util/SimpleGaugeNeedle.cs
@@ -20,20 +20,30 @@ namespace emptyLibUnity.UI.Util {
 	protected double rotation;
 	private double rotationZ = 15.2F;
 	private bool tilterIsSet = false; 
-
+	protected string[] styles = new string[]{"descending"};
+	public int selectedStyleKey = 0;
 	void Start () {
 		
 	}
 	
 	public void getTilter(double tiltVal){
 		this.tilter = tiltVal;
-//Debug.Log(this.tilter);
+Debug.Log(this.tilter);
 		this.setRotation();
 	}
 
 	protected void setRotation(){
 		//LIM farright, farleft = (x/cosY)*H
-		this.rotationZ = (this.tilter/this.farLeft)*this.farRight;
+		string current_style = this.styles[this.selectedStyleKey];
+		switch(current_style){
+			case "descending":
+					this.rotationZ = (this.tilter/this.farLeft)*this.farRight;//descending from center
+				break;
+			default:
+					this.rotationZ = (this.tilter/this.farLeft)*this.farRight;//descending from center
+				break;
+		}
+		
 	}
 
 	// Update is called once per frame
