fda7743bca004efc4618dc3c358d81b6b2c53f20
rgarro <rgarro@gmail.com>
2020-08-01 20:40:14 -0600

    Blondie versus Tuco, jesus on the sea sailing to alaska ..

--- emptyLibUnity/Controllable/Weapons/TurretController.cs
+++ emptyLibUnity/Controllable/Weapons/TurretController.cs
@@ -1,10 +1,10 @@
 /**
- *
+ * Blondie Versus Tuco, jesus on the sea sailing to alaska
  *
  * turns horizontally rotationSteps current transform
  * clockwise or counterclockwise from input turn key
  * 
- * @rolando <rolando@emptyart.xyz>
+ * @author Rolando <rgarro@gmail.com>
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -14,8 +14,8 @@ public class TurretController : MonoBehaviour {
 
 	public GameObject turretObj;
 	public GameObject barrelsObj;
-	
-
+	public float maxElevationAngle = 75;
+	public float minElevationAngle = 0;
 	void Start () {
 		
 	}
