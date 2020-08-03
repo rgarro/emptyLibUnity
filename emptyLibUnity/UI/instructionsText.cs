02f1b87d846b3d29e4f5f212dd0cbc6fbfdf0916
rgarro <rgarro@gmail.com>
2020-08-03 17:21:28 -0600

    taking the A train ...

---
+++ emptyLibUnity/UI/instructionsText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instructionsText : MonoBehaviour
+{
+    public float windowX = 10;
+    public float windowY = 65;
+    public float windowHeight = 170;
+    public float windowWidth = 150;
+    public string instructionsString = "Drive: Arrow Keys \n\nTurn Turret: F, G\n\nElevation: up R down T\n\nShoot: spacebar";
+    public string labelString = "Instructions";
+    public float marginVal = 25;
+    private Rect windowRect;
+
+    void OnGUI(){
+        this.windowRect = new Rect(this.windowX,this.windowY,this.windowHeight,this.windowWidth);
+        this.windowRect = GUI.Window(0,windowRect,WindowFunction,this.labelString);
+    }
+
+    void WindowFunction(int windowID){
+      //place instructions here ...
+      GUI.Label(new Rect(this.marginVal,this.marginVal,this.windowHeight,this.windowWidth),this.instructionsString);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
