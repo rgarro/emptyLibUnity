9a9584a91f5357ceb7f78275d80f0e4ae17a66d5
rgarro <rgarro@gmail.com>
2020-08-04 20:19:08 -0600

    the word polynomial comes from poly ...

--- emptyLibUnity/Controllable/Weapons/TurretController.cs
+++ emptyLibUnity/Controllable/Weapons/TurretController.cs
@@ -13,15 +13,11 @@ using UnityEngine;
 public class TurretController : MonoBehaviour {
 
 	public GameObject turretObj;
-	public GameObject barrelsObj;
-	public float maxElevationAngle = 75;
-	public float minElevationAngle = 0;
 	 public string leftTurnKey = "f";
     public string rightTurnKey = "g";
     public float rotationSteps = 0.38f;
 	private AudioSource servoSoundPlayer;
     public AudioClip servoSoundClip;
-	private float turretRotationY;
 	private float turretRotationZ;
 	public float farLeftTurretRotationAngle = -130;
 	public float farRightTurretRotationAngle = 118;
@@ -29,15 +25,59 @@ public class TurretController : MonoBehaviour {
     private bool gLeft = false;
 	//public float turretYpivot = 0;
 	//public float turretXpivot = 0;
+	public GameObject barrelsObj;
+	private float barrelsElevationY;
+	public float maxElevationAngle = 75;
+	public float minElevationAngle = 0;
+	public string upKey = "r";
+    public string downKey = "t";
+    public float elevationSteps = 0.37f;
+	private bool gUP = false;
+    private bool gDown = false;
 
 	void Start () {
 		this.turretRotationZ = this.turretObj.transform.rotation.z;
-		this.turretRotationY = this.turretObj.transform.rotation.y;
+		this.barrelsElevationY = this.barrelsObj.transform.rotation.y;
 		this.servoSoundPlayer = GetComponent<AudioSource> ();
 	}
 
 	void Update () {
-        if(Input.GetKeyDown(this.leftTurnKey)){
+        this.rotationKeys();
+		this.elevationKeys();
+	}
+
+	private void elevationKeys(){
+		//float elevRad = this.barrelsObj.transform.localEulerAngles.x;
+		if(Input.GetKeyDown(this.downKey)){
+            this.gDown = true;
+            this.gUP = false;
+			//if (elevRad < this.minElev) 
+			this.barrelsElevationY = (Mathf.Abs(this.barrelsElevationY) + this.elevationSteps)*-1; 
+        }
+		if(Input.GetKeyDown(this.upKey)){
+            this.gDown = false;
+            this.gUP = true;
+			//if (elevRad > this.maxElev)
+			this.barrelsElevationY = Mathf.Abs(this.barrelsElevationY) + this.elevationSteps; 
+        }
+		if(this.gDown || this.gUP){
+			this.elevateBarrels();
+		}
+		if (Input.GetKeyUp (this.downKey) || Input.GetKeyUp(this.upKey)) {
+            this.gDown = false;
+            this.gUP = false;
+            this.servoSoundPlayer.Stop ();
+        }
+	}
+
+	private void elevateBarrels(){
+		this.playServoSoundOn();
+//Debug.Log ("frankling marshall es un violador ...." + this.barrelsElevationY);
+		this.barrelsObj.transform.Rotate(0,this.barrelsElevationY,0);
+	}
+
+	private void rotationKeys(){
+		if(Input.GetKeyDown(this.leftTurnKey)){
            this.gLeft = true;
 		   this.gRight = false;
 			this.turretRotationZ = (Mathf.Abs(this.turretRotationZ) + rotationSteps)*-1;
@@ -54,15 +94,18 @@ public class TurretController : MonoBehaviour {
         }
         
         if (Input.GetKeyUp (this.rightTurnKey) || Input.GetKeyUp(this.leftTurnKey)) {
-            gRight = false;
-            gLeft = false;
+            this.gRight = false;
+            this.gLeft = false;
             this.servoSoundPlayer.Stop ();
         }
 	}
 
+	private float radianToDegree(float angle){
+        return angle * (180.0f / Mathf.PI);
+    }
+
 	private void rotateTurret(){
 		this.playServoSoundOn();
-Debug.Log ("here IM is only rocknroll ...." + this.turretRotationZ);
 		this.turretObj.transform.Rotate(0,0,this.turretRotationZ);
 		//Quaternion target = Quaternion.Euler(0,this.turretRotationY,this.turretObj.transform.rotation.z);
         //this.turretObj.transform.SetPositionAndRotation(this.turretObj.transform.position,target);
