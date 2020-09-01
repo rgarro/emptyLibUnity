f928ed75509a52a9faaee8945364699b2005af4a
rgarro <rgarro@gmail.com>
2020-08-31 19:44:26 -0600

    si te acordaras hoy de mi fuiste el primer amor mi vida entera ...

---
+++ emptyLibUnity/UI/rearViewMirrorResetButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *   __---~~~~--__                      __--~~~~---__
+ *  `\---~~~~~~~~\\                    //~~~~~~~~---/'
+ *    \/~~~~~~~~~\||                  ||/~~~~~~~~~\/
+ *                `\\                //'
+ *                  `\\            //'
+ *                    ||          ||
+ *          ______--~~~~~~~~~~~~~~~~~~--______
+ *     ___ // _-~                        ~-_ \\ ___
+ *    `\__)\/~                              ~\/(__/'
+ *     _--`-___                            ___-'--_
+ *   /~     `\ ~~~~~~~~------------~~~~~~~~ /'     ~\
+ *  /|        `\         ________         /'        |\
+ * | `\   ______`\_      \------/      _/'______   /' |
+ * |   `\_~-_____\ ~-________________-~ /_____-~_/'   |
+ * `.     ~-__________________________________-~     .'
+ *  `.      [_______/------|~~|------\_______]      .'
+ *   `\--___((____)(________\/________)(____))___--/'
+ *    |>>>>>>||                            ||<<<<<<|
+ *    `\<<<<</'                            `\>>>>>/'
+ *
+ * Displays ON /OFF button for rearView mirror
+ * more than one camera should be avoided on webGl
+ *
+ *@author Rolando<rgarro@gmail.com> 
+ */
+public class rearViewMirrorResetButton : MonoBehaviour
+{
+    public Texture2D rearViewMirrorIcon;
+    public GameObject rearViewMirrorObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
