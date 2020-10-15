93f38c0a9515b9090ed240bb3d0a866233a8250c
rgarro <rgarro@gmail.com>
2020-10-15 11:39:02 -0600

    burning bridges lost forever more ...

---
+++ emptyLibUnity/Controllable/Vehicles/Cars/LamboDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LamboDoor : MonoBehaviour
+{
+  public GameObject LeftDoor;
+  public GameObject RightDoor;
+  public float openedDoorAngle;
+  public float closedDoorAngle;
+  public integer doorSteps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void closeRightDoor()
+    {
+
+    }
+
+    void closeLeftDoor(){
+
+    }
+
+    void playOpenSound(){
+
+    }
+
+    void playCloseSound(){
+      
+    }
+}
