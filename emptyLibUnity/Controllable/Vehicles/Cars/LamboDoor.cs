23f165fdcbd6ffa7be2d8d88c241f41282773f5c
rgarro <rgarro@gmail.com>
2020-10-16 20:16:38 -0600

    Ïts been seven hours and fifteen days since they took your love away ..

--- emptyLibUnity/Controllable/Vehicles/Cars/LamboDoor.cs
+++ emptyLibUnity/Controllable/Vehicles/Cars/LamboDoor.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *                       ___..............._
+ *              __.. ' _'.""""""\\""""""""- .`-._
+ *  ______.-'         (_) |      \\           ` \\`-. _
+ * /_       --------------'-------\\---....______\\__`.`  -..___
+ * | T      _.----._           Xxx|x...           |          _.._`--. _
+ * | |    .' ..--.. `.         XXX|XXXXXXXXXxx==  |       .'.---..`.     -._
+ * \_j   /  /  __  \  \        XXX|XXXXXXXXXXX==  |      / /  __  \ \        `-.
+ * _|  |  |  /  \  |  |       XXX|""'            |     / |  /  \  | |          |
+ * |__\_j  |  \__/  |  L__________|_______________|_____j |  \__/  | L__________J
+ *     `'\ \      / ./__________________________________\ \      / /___________\
+ *        `.`----'.'   dp                                `.`----'.'
+ *          `""""'                                         `""""'
+ *
+ */
 public class LamboDoor : MonoBehaviour
 {
   public GameObject LeftDoor;
   public GameObject RightDoor;
   public float openedDoorAngle;
   public float closedDoorAngle;
-  public integer doorSteps;
+  public int doorSteps;
+  public AudioSource audioData;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        this.audioData = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
