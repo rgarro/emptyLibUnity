5be4f835a95916073d7c0b0206aa9e81e6956ba9
rolando <rgarro@gmail.com>
2021-05-18 16:09:53 -0600

    will be back ...

--- emptyLibUnity/Controllable/Vehicles/Drones/parrotBebopController.cs
+++ emptyLibUnity/Controllable/Vehicles/Drones/parrotBebopController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class parrotBebopController : MonoBehaviour
 {
-
+    
+    private Animator ani;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.ani.Play("HelipadsBL");
+        this.ani.Play("HelipadsBR");
+        this.ani.Play("HelipadsFL");
+        this.ani.Play("HelipadsFR");
     }
 }
