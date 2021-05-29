c89ca4a5808a33ca9341117a172fc293606e8057
rolando <rgarro@gmail.com>
2021-05-29 11:11:23 -0600

    mi general ortega les corto la pinga y la mando a enterrar al mar de aral ...

--- emptyLibUnity/Controllable/Vehicles/Helicopters/heliRotator.cs
+++ emptyLibUnity/Controllable/Vehicles/Helicopters/heliRotator.cs
@@ -19,9 +19,16 @@ public class heliRotator : MonoBehaviour
     void Update()
     {
         if(this.turnClocwise){
-            //this.heliPad.transform.rotation.z += Mathf.Abs(this.rotationSteps);
+            this.heliPad.transform.rotation.z += this.rotationSteps;
         } else {
-            //this.heliPad.transform.rotation.z += Mathf.Abs(this.rotationSteps) * -1;
+            this.heliPad.transform.rotation.z += this.rotationSteps * -1;
         }
+        /*
+        Vector3 lookPos = target. position - transform. position;
+Quaternion lookRot = Quaternion. LookRotation(lookPos, Vector3. up);
+float eulerY = lookRot. eulerAngles. y;
+Quaternion rotation = Quaternion. Euler (0, eulerY, 0);
+transform. rotation = rotation;
+        */
     }
 }
