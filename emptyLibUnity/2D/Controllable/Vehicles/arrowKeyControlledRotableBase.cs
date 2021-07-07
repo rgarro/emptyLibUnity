65e36e8f063625a343e48b7429ee48e1baafc5f9
rolando <rgarro@gmail.com>
2021-07-07 12:49:22 -0600

    the start of c mayor scale controlling a sprite with triangulated forward from a qwerty input

---
+++ emptyLibUnity/2D/Controllable/Vehicles/arrowKeyControlledRotableBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*  ░░░░░░███████ ]▄▄▄▄▄▄▄▄▃
+*  ▂▄▅█████████▅▄▃▂
+* I███████████████████].
+* ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...
+*
+* c mayor scale controlling a sprite with triangulated forward from a qwerty input  
+*
+*@author Rolando <rgarro@gmail.com>
+*/
+public class arrowKeyControlledRotableBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
