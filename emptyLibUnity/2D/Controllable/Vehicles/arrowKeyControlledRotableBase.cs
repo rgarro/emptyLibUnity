08eedda5564933d4079896c439eef44d0c588c45
rolando <rgarro@gmail.com>
2021-07-07 22:15:01 -0600

    the sun the sky they all are blue ...

--- emptyLibUnity/2D/Controllable/Vehicles/arrowKeyControlledRotableBase.cs
+++ emptyLibUnity/2D/Controllable/Vehicles/arrowKeyControlledRotableBase.cs
@@ -13,10 +13,17 @@ using UnityEngine;
 */
 public class arrowKeyControlledRotableBase : MonoBehaviour
 {
+
+    public float rotationSteps = 3.014f;
+    private GameObject TheBase;
+    private AudioSource servoSoundPlayer;
+	public AudioClip servoSoundClip;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.servoSoundPlayer = GetComponent<AudioSource>();
+        this.TheBase = GetComponent<GameObject>();
     }
 
     private void keyListeners(){
@@ -24,10 +31,51 @@ public class arrowKeyControlledRotableBase : MonoBehaviour
         {
             this.upArrowAction();
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.downArrowAction();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.rightArrowAction();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.leftArrowAction();
+        }
+    }
+
+
+     private void leftArrowAction(){
+        Debug.Log("left arrow action ...");
+        float rotate = this.TheBase.transform.rotation.z + this.rotationSteps;
+         this.TheBase.transform.Rotate(0,0,rotate);
+    }
+    private void rightArrowAction(){
+        Debug.Log("right arrow action ...");
+        float rotate = this.TheBase.transform.rotation.z + this.rotationSteps;
+        this.TheBase.transform.Rotate(0,0,rotate);
+        /*
+        If you have the hypotenuse, multiply it by sin(θ) to get the length of the side opposite to the angle.
+         Alternatively, multiply the hypotenuse by cos(θ) to get the side adjacent to the angle.
+          If you have the non-hypotenuse side adjacent to the angle, divide it by cos(θ) to get the length of the hypotenuse.
+        */
     }
 
     private void upArrowAction(){
         Debug.Log("upArrowAction here...");
+        this.playServoSoundOn();
+    }
+
+    private void downArrowAction(){
+        Debug.Log("downArrowAction here...");
+    }
+
+    private void playServoSoundOn(){
+        this.servoSoundPlayer.clip = this.servoSoundClip;
+        if (!this.servoSoundPlayer.isPlaying) {
+            this.servoSoundPlayer.Play ();
+        }
     }
 
     // Update is called once per frame
