cf714f7faf9831f6e7cb1a7c322a87673464fde0
rolando <rgarro@gmail.com>
2021-07-18 09:47:45 -0600

    glue stick waiting down , hommer simpson counting corn , satan hotrod thing ...

---
+++ emptyLibUnity/2D/Controllable/Vehicles/sumMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sumMover : MonoBehaviour
+{
+    public float stepsDown = 0.015f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
