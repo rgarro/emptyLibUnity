3330327e1f5150f0355ec3c8df5895593c07795d
rolando <rgarro@gmail.com>
2021-08-02 18:19:30 -0600

    algeria de los moros esta lejos ...

---
+++ emptyLibUnity/Util/destroyByYpostion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyByYpostion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float destroyAt = -5.01f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y < this.destroyAt){
+            Destroy(this.gameObject);
+        }
+    }
+}
