0c92e8d5a8179fadd7828571ac925c2b77323310
rolando <rgarro@gmail.com>
2021-08-14 03:42:18 -0600

    midnight blues vibrations ...

--- emptyLibUnity/2D/enemyLiner.cs
+++ emptyLibUnity/2D/enemyLiner.cs
@@ -26,24 +26,11 @@ public class enemyLiner : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 	public float distanceFromSpanwnX = 100; 
-	public int Score;
-	//public dashBoard theDashBoard;
-	protected string theScore;
 	public GUISkin btnSkin;
 
-	//public Texture2D RestartIcon;
-	//public Texture2D dashBoardPicIcon;
-	//public float dbPicX = 600;
-	//public float dbPicY = 10;
-	//public int count = 0;
-	public float windowX = 10;
-    public float windowY = 65;
-    public float windowHeight = 170;
-    public float windowWidth = 150;
-	private Rect windowRect;
+	
 
 	void Start(){
-		this.UpdateScore();
 		StartCoroutine(spawnWaves());
 	}
 
@@ -55,7 +42,6 @@ Debug.Log("spawning waves ...");
 				Vector3 spawnPosition = new Vector3 (Random.Range (spawnValues.x - distanceFromSpanwnX, spawnValues.x + distanceFromSpanwnX), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
-				//Instantiate (hazard, spawnPosition);
 				
 				yield return new WaitForSeconds (spawnWait);
 			}
@@ -63,18 +49,6 @@ Debug.Log("spawning waves ...");
 		}
 	}
 
-	void OnGUI(){
-		this.windowRect = new Rect(this.windowX,this.windowY,this.windowHeight,this.windowWidth);		
-		GUI.Label(this.windowRect,this.theScore);
-	}
-
-	public void addScore(int scoreValue){
-		Score += scoreValue;
-		UpdateScore ();
-	}
-
-	void UpdateScore(){
-		this.theScore = Score  + " Points";
-	}
+	
 
 }
---
+++ emptyLibUnity/UI/scoreDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreDisplay : MonoBehaviour
+{
+    public float windowX = 10;
+    public float windowY = 65;
+    public float windowHeight = 170;
+    public float windowWidth = 150;
+	private Rect windowRect;
+    public int Score =0;
+    protected string theScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void increaseScore(){
+
+    }
+
+    void OnGUI(){
+		this.windowRect = new Rect(this.windowX,this.windowY,this.windowHeight,this.windowWidth);		
+		GUI.Label(this.windowRect,this.theScore);
+	}
+
+	public void addScore(int scoreValue){
+		Score += scoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore(){
+		this.theScore = Score  + " Points";
+	}
+}
