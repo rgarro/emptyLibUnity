c128696472dc0fc7e20f0e1da95889cb8ba99acf
rgarro <rgarro@gmail.com>
2021-10-30 09:30:35 -0600

    Im a material gir in a materiail world ...

--- emptyLibUnity/2D/Controllable/shootController.cs
+++ emptyLibUnity/2D/Controllable/shootController.cs
@@ -12,6 +12,7 @@ public class shootController : MonoBehaviour
 
     public GameObject roundObject;
     public GameObject gunObject;
+    public int shoots = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +29,13 @@ public class shootController : MonoBehaviour
     private void trigger(){
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("trigger pulled ..");
+            this.shoot();
+            this.shoots = this.shoots + 1;
+        }
+    }
+
+    private void shoot(){
+         Debug.Log("trigger pulled ..");
             Debug.Log(" ++X:"+this.gunObject.transform.position.x);
             
             Vector3 spawnPosition = new Vector3 (this.gunObject.transform.position.x,this.gunObject.transform.position.y,this.gunObject.transform.position.z);
@@ -36,6 +43,5 @@ public class shootController : MonoBehaviour
 			spawnRotation = this.gunObject.transform.rotation;
             Debug.Log(" ++Rot:"+spawnRotation);
             Instantiate (roundObject, spawnPosition, spawnRotation);
-        }
     }
 }
