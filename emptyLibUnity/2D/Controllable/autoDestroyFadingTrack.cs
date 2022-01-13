fc505ce498f92c05f5cb25bd451caeffa7ca5a3b
rgarro <rgarro@gmail.com>
2022-01-13 17:43:59 -0600

    jazz liberator ...

---
+++ emptyLibUnity/2D/Controllable/autoDestroyFadingTrack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoDestroyFadingTrack : MonoBehaviour
+{
+
+     public float secondsToLive = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.countDown();
+    }
+
+     void countDown(){
+        if(this.secondsToLive > 0){
+            this.secondsToLive = this.secondsToLive - Time.deltaTime;
+        }else{
+            Destroy(this.gameObject);
+        }
+    }
+}
