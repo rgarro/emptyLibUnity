7784140dcf9281b5da02cf66795ac442df4ad4e1
rgarro <rgarro@gmail.com>
2022-01-14 07:58:55 -0600

    Andrea era una modelo herediana ...

--- emptyLibUnity/2D/Controllable/autoDestroyFadingTrack.cs
+++ emptyLibUnity/2D/Controllable/autoDestroyFadingTrack.cs
@@ -1,11 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *       .-""""--.
+ *      /         )
+ *     /      --"`
+ *    /       _`:---.
+ *   |     .-'       `\
+ *    \   /    .----'./
+ *      \  : ,-' ~(.).)\
+ *      \_| \      ._) |
+ *       /   |  \.__, /
+ *  _.--'    )`///-,-'
+ * /        / _| (_\\
+ *|        (____/____)
+ * \     ___/       | _
+ *  `---(            ` )
+ *       `-,          .'
+ *        (__.'._/'._/
+ *             |`| |
+ *          __/ / /
+ *         //   | `--.
+ *        ||    /_____)
+ *  jgs   `=---`
+ * Autodestroy Countdown with Fade on SpriteRenderer 2D
+ * 
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class autoDestroyFadingTrack : MonoBehaviour
 {
 
      public float secondsToLive = 3.0f;
+     public float alphaLevel = 1.0f;
+     public float transparencyRate = .011f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +49,9 @@ public class autoDestroyFadingTrack : MonoBehaviour
      void countDown(){
         if(this.secondsToLive > 0){
             this.secondsToLive = this.secondsToLive - Time.deltaTime;
+            this.alphaLevel -= this.transparencyRate;
+            GetComponent<SpriteRenderer>().color = new Color(1,1,1,this.alphaLevel);
+            //GetComponent<SpriteRenderer> ().color.a = ;
         }else{
             Destroy(this.gameObject);
         }
