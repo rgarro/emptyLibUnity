165cb50d060e0c2683d662be413d66eb09eaf764
rgarro <rgarro@gmail.com>
2022-01-17 10:02:53 -0600

    mi cabhrina contra debe aparecer en amazon para pagarlo con la tarjeta de essential costarica

--- emptyLibUnity/UI/restDBIOGameStarter.cs
+++ emptyLibUnity/UI/restDBIOGameStarter.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
-*       ___------__
-* |\__-- /\       _-
-* |/    __      -
-* //\  /  \    /__
-* |  o|  0|__     --_
-* \\____-- __ \   ___-
-* (@@    __/  / /_
-*    -_____---   --_
-*     //  \ \\   ___-
-*   //|\__/  \\  \
-*   \_-\_____/  \-\
-*        // \\--\| 
-*   ____//  ||_
-*  /_____\ /___\
-*______________________
-* will promp player name and save it game into restdb.io before game starts
-* saves scores and displays game history from restdb.io
-*
-*@athor Rolando<rgarro@gmail.com>
-*/
+ *       ___------__
+ * |\__-- /\       _-
+ * |/    __      -
+ * //\  /  \    /__
+ * |  o|  0|__     --_
+ * \\____-- __ \   ___-
+ * (@@    __/  / /_
+ *    -_____---   --_
+ *     //  \ \\   ___-
+ *   //|\__/  \\  \
+ *   \_-\_____/  \-\
+ *        // \\--\| 
+ *   ____//  ||_
+ *  /_____\ /___\
+ *______________________
+ * will prompt player name and save it game into restdb.io before game starts
+ * saves scores and displays game history from restdb.io
+ * Fuck firebase , heroku and its eternal postgres instances
+ *
+ *@athor Rolando<rgarro@gmail.com>
+ */
 public class restDBIOGameStarter : MonoBehaviour
 {
 
@@ -31,6 +32,7 @@ public class restDBIOGameStarter : MonoBehaviour
     public int PlayerNameWidth = 100;
     public int PlayerNameHeight = 30;
     private string typedPlayerName = "";
+    public string buttonLegend = "Start Game";
     // Start is called before the first frame update
     void Start()
     {
@@ -40,6 +42,10 @@ public class restDBIOGameStarter : MonoBehaviour
     void OnGUI () 
     {
         this.typedPlayerName = GUI.TextField (new Rect (this.PlayerNameXPos, this.PlayerNameYPos,this.PlayerNameWidth, this.PlayerNameHeight), this.playerName);
+        if (GUI.Button(new Rect(this.PlayerNameXPos+100, this.PlayerNameYPos,this.PlayerNameWidth, this.PlayerNameHeight),this.buttonLegend)){
+            Debug.Log("Clicked the button with text");
+            Debug.Log(this.typedPlayerName);
+        }
     }
 
     // Update is called once per frame
