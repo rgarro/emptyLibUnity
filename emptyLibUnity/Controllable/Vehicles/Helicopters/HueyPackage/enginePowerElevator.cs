181cc057257594c6c1acf3891b2e77141b4b7bce
rgarro <rgarro@gmail.com>
2022-04-04 07:03:18 -0500

    Gina dreams of running away ...

---
+++ emptyLibUnity/Controllable/Vehicles/Helicopters/HueyPackage/enginePowerElevator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * Increases engine power rotation speed and helicopter elevation
+ *
+ *
+ *
+ */
+public class enginePowerElevator : MonoBehaviour
+{
+    public float initialAltitude = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
