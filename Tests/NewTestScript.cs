8faf1fb2d52db6c1bf8817ac016895707591cf01
rgarro <rgarro@gmail.com>
2022-04-26 14:01:18 -0500

    till the end A forest mighty black ...

--- Tests/NewTestScript.cs
+++ Tests/NewTestScript.cs
@@ -12,12 +12,12 @@ public class NewTestScript {
        Assert.IsTrue( em.is_empty );
 
     }
-    public void NewTestScriptSimplePasses() {
+   /* public void NewTestScriptSimplePasses() {
         int[] array = new int[] { 1, 2, 3 };
 Assert.That(array, Has.Exactly(1).EqualTo(3));
 Assert.That(array, Has.Exactly(2).GreaterThan(1));
 Assert.That(array, Has.Exactly(3).LessThan(100));
-    }
+    }*/
 
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
