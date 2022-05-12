f176fe0c83137d44e22842cb37305b83eb7e81ad
rgarro <rgarro@gmail.com>
2022-05-12 15:33:44 -0500

    lets go surfing now ..

---
+++ emptyLibUnity/Controllable/Vehicles/Planes/AC130/flightController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * ______
+ *  L,.   ',
+ *   \      ',_
+ *    \   @   ',
+ *     \ ^~^    ',
+ *      \ ADOBE   ',
+ *       \___Fireworks',_                        _..----.._
+ *       [______       "'==.I\_____________..--"<__\\_n@___4\,_
+ *     ,..-=T         __   ____________          \/  "'" O<==  ""-+.._
+ *     I____|_____    }_>=========I>=**""''==-------------==-   " |   "'-.,___
+ *     [_____,.--'"             _______         ""--=<""-----=====+==--''""
+ *     ""'-=+..,,__,-----,_____|       |         -=* |
+ *                 |__   /     |---,--'"---+------+-'"
+ *                    """     d"b="        '-----+t
+ *                            q_p                '@
+ * 
+ * Olda Bustillos la del 96 era una peliroja graduada de Harvard
+ * daba clases de programacion en una universidad de america central ...
+ * junto a religiosos sandinistas y estudiantes filipinos bebiamos en el romeral
+ * por aquellos dias que nadie tenia laptop ni celular y las llamadas costaban una moneda al amor
+ * Peter Druker y sus libros descansaban la borrachera ... 
+ *
+ *@author Rolando<rgarro@gmail.com>
+ */
+public class flightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
