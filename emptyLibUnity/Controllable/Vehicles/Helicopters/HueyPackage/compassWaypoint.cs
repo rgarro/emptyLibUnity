e2d5d9cd638a38d69a4ec9d1beef2cb6720b794a
rgarro <rgarro@gmail.com>
2022-06-05 15:40:32 -0500

    small time shot away ...

--- emptyLibUnity/Controllable/Vehicles/Helicopters/HueyPackage/compassWaypoint.cs
+++ emptyLibUnity/Controllable/Vehicles/Helicopters/HueyPackage/compassWaypoint.cs
@@ -1,9 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ * Will Test KiteSurf Gear for Money 
+ *
+ *                ,,,,,,,,,,---''''---,,,,,,,,,,
+ *      --''''''''          ````][''''          ''''''''--
+ *                           _3'''':.
+ * _                  .,---------------.
+ * \\    / _________./  |[__]| o   |J@"\\__
+ *  \\==o=========:;    |____|[IL__|''''/_/')
+ *     /            `'-,._____===\=_____.,-'
+ *                          \         \     ,
+ *                    """"""""""""""""""""""
+ * Displays Compass String From given object
+ *
+ *
+ *@author Rolando<rgarro@gmail.com>
+ */
 public class compassWaypoint : MonoBehaviour
 {
+
+    public GameObject helicopter;
+
     // Start is called before the first frame update
     void Start()
     {
