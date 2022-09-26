bdbab47321d902fd2b8d1d547c46c790cce2d9e6
rgarro <rgarro@gmail.com>
2022-09-26 00:03:53 -0500

    wind swell with 6 ft waves hitting the point II...

--- emptyLibUnity/Controllable/Vehicles/Planes/AC130/gunCameraController.cs
+++ emptyLibUnity/Controllable/Vehicles/Planes/AC130/gunCameraController.cs
@@ -1,13 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *                            ______
+ *         |\_______________ (_____\\______________
+ * HH======#H###############H#######################
+ *         ' ~""""""""""""""`##(_))#H\"""""Y########
+ *                           ))    \#H\       `"Y###
+ *                           "      }#H)
+ * The Wind Swell is two days in the future, I hid a chronic joint to smoke after having
+ * taken five waves. I went there today, the manatis where whispering secret mantras
+ * Lord Kanhapa is going to be there for five minutes, several nagpas from Boulder
+ * are going to come with machigun relics from man slaugters
+ * I saw a F22 climb 4000 meters in 3 seconds, that means aliens are on earth
+ * the f22 engine can summon gas from black waters to tanks recovering the water
+ * Saddam Hussein Migs never cleaned Tikrit so he lost his contract ...
+ * Sad Hill cementery is not far from snowbird, theres an outlet mall there ...
+ * 
+ *
+ *
+ *@author rolando<rolando@emptyart.xyz>
+ */
 public class gunCameraController : MonoBehaviour
 {
+    public GameObject Gun;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void moveUp(){
+
+    }
+
+    void moveDown(){
+
     }
 
     // Update is called once per frame
