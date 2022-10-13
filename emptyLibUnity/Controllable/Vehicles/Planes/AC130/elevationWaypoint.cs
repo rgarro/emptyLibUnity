a0974150bf45d73e345da57d9b41e6de7abd9a70
rgarro <rgarro@gmail.com>
2022-10-13 09:15:23 -0500

    Caro Quintero esta en Nayarit ..

---
+++ emptyLibUnity/Controllable/Vehicles/Planes/AC130/elevationWaypoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*
+* Una paloma ha caido en la laguna de Nuzco 
+* porque no pudo bajar en la pista de acapulco
+* un gavilan federal le venia siguiendo el rumbo ..
+*
+*/
+public class elevationWaypoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
