fb5052b6d1b1618702de59314f9acdd5e0157f8f
rgarro <rgarro@gmail.com>
2022-10-14 08:52:14 -0500

    slerping chill out sounds on midi efects ...

--- emptyLibUnity/Controllable/Vehicles/Planes/AC130/elevationWaypoint.cs
+++ emptyLibUnity/Controllable/Vehicles/Planes/AC130/elevationWaypoint.cs
@@ -2,19 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
-*
-* Una paloma ha caido en la laguna de Nuzco 
-* porque no pudo bajar en la pista de acapulco
-* un gavilan federal le venia siguiendo el rumbo ..
-*
-*/
+ *         \    |\
+ *         \   | \
+ *         ____| |__
+ *         \___ \|__\
+ *             \  \
+ *              |  \
+ *              \ * \_
+ *     \         \  \ \     \
+ *      \        /\  \|\ _   \
+ *      ________/  X  ~~//_____
+ *      \_________/ \  //|_____\
+ *              *    \/o/  *
+ *               *   //     *
+ *                   ~
+ *                 *           *
+ *
+ * Una paloma ha caido en la laguna de Nuzco 
+ * porque no pudo bajar en la pista de acapulco
+ * un gavilan federal le venia siguiendo el rumbo ..
+ *
+ *@author Rolando<rolando@emptyart.xyz>
+ */
 public class elevationWaypoint : MonoBehaviour
 {
     public GameObject Airplane;
-    private string frontDegreesQty;
+    private string topAltQty;
     public int topLabelXpos = 25;
     public int topLabelYpos = 25;
-    
+     private string topLetter = "ALT";
+
+    private string middleAltQty;
+    public int middleLabelXpos = 800;
+    public int middleLabelYpos = 25;
+    private string leftLetter = "Mtr";
+
+     private string lowerAltQty;
+    public int lowerLabelXpos = 400;
+    public int lowerLabelYpos = 25;
+    private string downlLetter = "Mtr";
+
+
     // Start is called before the first frame update
     void Start()
     {
