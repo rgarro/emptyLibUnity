031c87dd59d32975c65d7c9054aac7fafab0ea16
rgarro <rgarro@gmail.com>
2023-04-17 04:52:48 -0500

    deep cleansing ..

---
+++ emptyLibUnity/Controllable/Vehicles/Planes/F22/f22FlightController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * ______
+ *  L,.   ',
+ *   \      ',_
+ *    \   @   ',
+ *     \ ^~^    ',
+ *      \ ADOBE   ',
+ *       \___Fireworks',_                        _..----.._
+ *       [______       "'==.I\_____________..--"<__\\_n@___4\,_
+ *     ,..-=T         __   ____________          \/  "'" O<==  ""-+.._
+ *     I____|_____    }_>=========I>=**""''==-------------==-   " |   "'-.,___
+ *     [_____,.--'"             _______         ""--=<""-----=====+==--''""
+ *     ""'-=+..,,__,-----,_____|       |         -=* |
+ *                 |__   /     |---,--'"---+------+-'"
+ *                    """     d"b="        '-----+t
+ *                            q_p                '@
+ * Mi Coronel Kaddafi esta en Macondo ...
+ *
+ *@author Rolando<rolando@emptyart.xyz>
+ */
+public class f22FlightController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
