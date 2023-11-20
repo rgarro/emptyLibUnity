a6bbffc896f8c20cc4dc25c9c4eb8178c8d16a98
rgarro <rgarro@gmail.com>
2023-11-20 15:03:19 -0600

    blow up the outside world ..

---
+++ emptyLibUnity/Controllable/Rail/simpleGTLocomotive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *         o x o x o x o . . .
+ *         o      _____            _______________ ___=====__T___
+ *       .][__n_n_|DD[  ====_____  |    |.\/.|   | |   |_|     |_
+ *      >(________|__|_[_________]_|____|_/\_|___|_|___________|_|
+ *      _/oo OOOOO oo`  ooo   ooo   o^o       o^o   o^o     o^o
+ * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ * No voy en Tren Voy en Avion ..
+ *
+ *
+ *@author Rolando<rgarro@gmail.com>
+ */
+public class simpleGTLocomotive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
