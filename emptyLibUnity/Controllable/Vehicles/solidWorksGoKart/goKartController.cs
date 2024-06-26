cf180498d0c2e9f45fe938fe58a17ccb010a89f4
rgarro <rgarro@gmail.com>
2024-06-26 10:14:23 -0600

    Plucker vectors and skew matrices

--- emptyLibUnity/Controllable/Vehicles/solidWorksGoKart/goKartController.cs
+++ emptyLibUnity/Controllable/Vehicles/solidWorksGoKart/goKartController.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *     ---------------------------.
+ *  `/""""/""""/|""|'|""||""|   ' \.
+ *  /    /    / |__| |__||__|      |
+ * /----------=====================|
+ * | \  /V\  /    _.               |
+ * |()\ \W/ /()   _            _   |
+ * |   \   /     / \          / \  |-( )
+ * =C========C==_| ) |--------| ) _/==] _-{_}_)
+ *  \_\_/__..  \_\_/_ \_\_/ \_\_/__.__.
+ * 
+ * Allows to control a gokart from the engine camshaft rotation and the wheel tube.
+ *
+ *
+ *
+ *@author Rolando<rgarro@gmail.com>
+ */
 public class goKartController : MonoBehaviour
 {
+
+    public float initialPower = 1.38f;
+    public GameObject frontWheel;
+
     // Start is called before the first frame update
     void Start()
     {
