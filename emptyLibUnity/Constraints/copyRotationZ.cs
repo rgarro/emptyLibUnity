9149da85a68a2300ec9ee84574dd0065a51a04bb
rgarro <rgarro@gmail.com>
2024-06-28 09:13:04 -0600

    whos got the bacon

---
+++ emptyLibUnity/Constraints/copyRotationZ.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyRotationZ : MonoBehaviour
+{
+    public GameObject fromObject;
+    public GameObject toObject;
+
+    private float fromDegrees;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void getFromRotation(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
