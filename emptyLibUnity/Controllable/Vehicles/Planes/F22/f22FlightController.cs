7c2a65c25613c033186ee103480eb3cd99ebaed2
rgarro <rgarro@gmail.com>
2024-07-22 16:49:36 -0600

    eyes without a face theres no human grace ..

--- emptyLibUnity/Controllable/Vehicles/Planes/F22/f22FlightController.cs
+++ emptyLibUnity/Controllable/Vehicles/Planes/F22/f22FlightController.cs
@@ -141,21 +141,23 @@ this.Cockpit.transform.Rotate(0,0,this.degreesToCloseCokpit);
     //must elevate with arrow button
     void descend(){
         if(this.AirPlane.transform.position.y > this.minAltitude){
-            this.AirPlane.transform.Translate(Vector3.back * (Time.deltaTime * this.yardsPerSecond));
+            //this.AirPlane.transform.Translate(Vector3.back * (Time.deltaTime * this.yardsPerSecond));
+            this.AirPlane.transform.Translate(Vector3.down * (Time.deltaTime * this.yardsPerSecond));
         }
     }
 
     //must descend with arrow button
     void elevate(){
         if(this.AirPlane.transform.position.y < this.maxAltitude){
-            this.AirPlane.transform.Translate(Vector3.forward * (Time.deltaTime * this.yardsPerSecond));
+            //this.AirPlane.transform.Translate(Vector3.forward * (Time.deltaTime * this.yardsPerSecond));
+            this.AirPlane.transform.Translate(Vector3.up * (Time.deltaTime * this.yardsPerSecond));
         }
     }
 
     void diveLeft(){
         //Debug.Log(" Diving Left ....");
-        //this.AirPlane.transform.Translate(Vector3.left * Time.deltaTime* (this.yardsPerSecond/this.sideDiveAccelerationRate));
-        this.AirPlane.transform.Translate(Vector3.left * Time.deltaTime* 2);
+        this.AirPlane.transform.Translate(Vector3.left * Time.deltaTime* (this.yardsPerSecond/this.sideDiveAccelerationRate));
+        //this.AirPlane.transform.Translate(Vector3.left * Time.deltaTime* 2);
         /*if(!this.isDived){
             this.AirPlane.transform.Rotate(0,0,this.diveCurveAngleZ*-1);
             this.isDived = true;
@@ -164,10 +166,10 @@ this.Cockpit.transform.Rotate(0,0,this.degreesToCloseCokpit);
 
     void diveRight(){
         this.AirPlane.transform.Translate(Vector3.right * Time.deltaTime* (this.yardsPerSecond/this.sideDiveAccelerationRate));
-        if(!this.isDived){
+        /*if(!this.isDived){
             this.AirPlane.transform.Rotate(0,0,this.diveCurveAngleZ);
             this.isDived = true;
-        }
+        }*/
     }
 
     void increaseSpeed(){
