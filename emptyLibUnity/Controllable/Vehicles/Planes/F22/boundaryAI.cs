11c36c22e30cd34380fd604a4f794d28d867e084
rgarro <rgarro@gmail.com>
2024-08-23 12:47:34 -0600

    Im gonna put on a iron shirt and chase Satan out of Earth ..

--- emptyLibUnity/Controllable/Vehicles/Planes/F22/boundaryAI.cs
+++ emptyLibUnity/Controllable/Vehicles/Planes/F22/boundaryAI.cs
@@ -1,7 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ *                     `. ___
+ *                    __,' __`.                _..----....____
+ *        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+ *  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+ *,'________________                          \`-._`-','
+ * `._              ```````````------...___   '-.._'-:
+ *    ```--.._      ,.                     ````--...__\-.
+ *            `.--. `-`                       ____    |  |`
+ *              `. `.                       ,'`````.  ;  ;`
+ *                `._`.        __________   `.      \'__/`
+ *                   `-:._____/______/___/____`.     \  `
+ *                               |       `._    `.    \
+ *                               `._________`-.   `.   `.___
+ *                                                  `------'`
+ * 
+ * King Mosiah ordered Alma the highest priest yea
+ * to expell all the lamanites possing as jahova witnesess from the land of Zarahemla yea
+ * king Mosiah warriors slained them all near by the turtle creek road 
+ * the king sent the orders from a piece remainder from the original lord Nephi's spaceship Amen ...
+ *
+ *  BE AWARE :
+ *
+ * this script will keep away lamanites away from your businnes but ,lucifer the king of the morning
+ * will define the boundaries until you pay him with with nectar of the sun  yea   
+ *
+ *
+ *@author Rolando<rolando@emptyart.xyz>
+ */
 public class boundaryAI : MonoBehaviour
 {
 
