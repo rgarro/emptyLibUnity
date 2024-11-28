250789b3ea3a6578147f99eaa80a55e34db4b3d1
rgarro <rgarro@gmail.com>
2024-11-28 11:05:01 -0600

    black friday just got hotter ..

---
+++ emptyLibUnity/AI/prefabTimeToLive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabTimeToLive : MonoBehaviour
+{
+    public int seconds_to_live;
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("tiramos ...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += 1.0F * Time.deltaTime;
+		//Debug.Log ("tiramos ..." + timer);
+		if (timer >= seconds_to_live)
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
