73990a8b46f483ff9ce4247f1de011e7d8f63d0e
rgarro <rgarro@gmail.com>
2024-12-23 09:13:06 -0600

    massive attack disolved girl ..

---
+++ emptyLibUnity/AI/enemyRocketDetonator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~oo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                                 o o
+ *                                 o ooo
+ *                                   o oo
+ *                                      o o      |   #)
+ *                                       oo     _|_|_#_    -la mota colombiana la venden 200 al sur de la clinica biblica-
+ *                                         o   | 751   |
+ *    __                    ___________________|       |_________________
+ *   |   -_______-----------                                              \
+ *  >|    _____                                                   --->     )
+ *   |__ -     ---------_________________________________________________ /
+ *
+ * Enterrar toneladas de mota en minas de chile  dejandolas aplastadas con troncos de pochote de hojancha
+ * hace que a los 200 anos la mota petrificada sirva de microchip en los iphones de las putas de nicoya
+ * Ambar de mota petrificada es lo que vende frankling chang en una plataforma espacial
+ * hay mota petrificada en la carcel, mono,taliban , bolincha,morgan ,henry,luisca ,guiller,tarzan , gato y perra han fumado esa piedra.
+ * A nelson hofman lo vacilan de que en las camaras de hola juventud tenian ambar de mota tica majado en los frios de groenlandia, por eso le sonaba la voz resfriada
+ * Tomas Guardia tenia una finca en Denver , majaba mota con miles de toneladas de nieve y la vendia raspada en polvo en los helados dospinos y pops
+ * en las licoreras de heredia venden vapes de cbd si uno pregunta descaradamente
+ * El ejercito sandinista le disparaba obuses de mota petrificada a los contras , que desertaban y se venian a chepe a monchar.
+ * Tirar piedras de mota petrificada en el crater del volcan arenal hacia nubes que pijiaban canas y tilaran.
+ * Bombear humo de mota petrificada a 800 metros de profunfidad hace que exista el plasma pijiante, frankling chang vendia fumar mota en orbita
+ *
+ *
+ *@author Rolando<rgarro@gmail.com>
+ */
+public class enemyRocketDetonator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
