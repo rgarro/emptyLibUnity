7080ed0e1cef4e506ef974635d1d9eb80a939743
rgarro <rgarro@gmail.com>
2024-12-24 10:48:31 -0600

    relaxing with cherry sour diesel visions sacred horses flying ..

--- emptyLibUnity/AI/enemyRocketDetonator.cs
+++ emptyLibUnity/AI/enemyRocketDetonator.cs
@@ -14,16 +14,7 @@ using UnityEngine;
  *  >|    _____                                                   --->     )
  *   |__ -     ---------_________________________________________________ /
  *
- * Enterrar toneladas de mota en minas de chile  dejandolas aplastadas con troncos de pochote de hojancha
- * hace que a los 200 anos la mota petrificada sirva de microchip en los iphones de las putas de nicoya
- * Ambar de mota petrificada es lo que vende frankling chang en una plataforma espacial
- * hay mota petrificada en la carcel, mono,taliban , bolincha,morgan ,henry,luisca ,guiller,tarzan , gato y perra han fumado esa piedra.
- * A nelson hofman lo vacilan de que en las camaras de hola juventud tenian ambar de mota tica majado en los frios de groenlandia, por eso le sonaba la voz resfriada
- * Tomas Guardia tenia una finca en Denver , majaba mota con miles de toneladas de nieve y la vendia raspada en polvo en los helados dospinos y pops
- * en las licoreras de heredia venden vapes de cbd si uno pregunta descaradamente
- * El ejercito sandinista le disparaba obuses de mota petrificada a los contras , que desertaban y se venian a chepe a monchar.
- * Tirar piedras de mota petrificada en el crater del volcan arenal hacia nubes que pijiaban canas y tilaran.
- * Bombear humo de mota petrificada a 2800 metros de profunfidad en el oceano hace que exista el plasma pijiante, 
+ * on collide enemy rockets will subtract life points to collided 
  *
  *
  *@author Rolando<rgarro@gmail.com>
@@ -40,5 +31,12 @@ public class enemyRocketDetonator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Output the Collider's GameObject's name
+        Debug.Log("colision here: ");
+        Debug.Log(collision.collider.name);
     }
 }
