f8a10665dc663bc5d0a288fb21faa912dee98bd1
rgarro <rgarro@gmail.com>
2024-12-26 11:28:37 -0600

    later out with thw 85 mustang one time got me on the radar ..

--- emptyLibUnity/AI/enemyRocketDetonator.cs
+++ emptyLibUnity/AI/enemyRocketDetonator.cs
@@ -8,20 +8,22 @@ using UnityEngine;
  *                                 o ooo
  *                                   o oo
  *                                      o o      |   #)
- *                                       oo     _|_|_#_    -la mota colombiana la venden 200 al sur de la clinica biblica-
+ *                                       oo     _|_|_#_    - el ara san juan recoge metano de las aguas negras de moin  -
  *                                         o   | 751   |
  *    __                    ___________________|       |_________________
  *   |   -_______-----------                                              \
- *  >|    _____                                                   --->     )
+ *  >|    _____                                 SS RECOPE         --->     )
  *   |__ -     ---------_________________________________________________ /
  *
- * on collide enemy rockets will subtract life points to collided 
+ * on collide enemy rockets will subtract life points to collided and trigger explosion object
  *
  *
  *@author Rolando<rgarro@gmail.com>
  */
 public class enemyRocketDetonator : MonoBehaviour
 {
+
+    public string enemyObjectTag = "perroNegro";
     // Start is called before the first frame update
     void Start()
     {
@@ -39,8 +41,8 @@ public class enemyRocketDetonator : MonoBehaviour
         //Output the Collider's GameObject's name
         //Debug.Log("colision BEGIN: ");
         Debug.Log("colision: "+ collision.gameObject.tag);
-        if(collision.gameObject.tag == "mi28Prefab"){
-            Debug.Log("colision: "+ collision.gameObject.tag);
+        if(collision.gameObject.tag == this.enemyObjectTag){
+            Debug.Log("tome chichi : "+ collision.gameObject.tag);
         }
         //Debug.Log(collision.collider.name);
         //Debug.Log("colision END ");
