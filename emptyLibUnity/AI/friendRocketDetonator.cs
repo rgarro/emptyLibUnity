bf42bda5a054412fb124a0b46e82f643f36c280a
rgarro <rgarro@gmail.com>
2025-01-04 14:33:29 -0600

    blue train and coltrane foil downwind tivives ...

--- emptyLibUnity/AI/friendRocketDetonator.cs
+++ emptyLibUnity/AI/friendRocketDetonator.cs
@@ -7,8 +7,7 @@ using UnityEngine;
  *  ,%%%______|O|
  *  %%%/_________\
  *  `%%| /\[][][]|%
- * ___||_||______|%&,__ hjw
- *henry es playo, quincho es gay, mamerto es gay , bolincha es gay, ya no tienen gracia y los caga el espiritu santo 
+ * ___||_||______|%&,__ hjw 
  *
  * will detect round collitions on player and increase player damage
  *
@@ -18,10 +17,36 @@ using UnityEngine;
  */
 public class friendRocketDetonator : MonoBehaviour
 {
+     public string enemyObjectTag = "maestroRoshi";
+    public string scoreManagerTag = "BatComputer";
+    //private int downedTargetCount = 0;
+    private GameObject damageCountdown;
+    public int ptsToIncrease = 10;
+    public bool isDamage = false;
+     public GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.getDamageManager();
+    }
+
+    void getDamageManager(){
+        this.damageCountdown = GameObject.FindWithTag(this.scoreManagerTag);
+    }
+
+    void tomeChichi(Collision collision){
+        Debug.Log("fume mota y lea poesia : "+ collision.gameObject.tag);
+        //this.increaseScore();
+        //GameObject e = Instantiate(this.explosion) as GameObject;
+        //e.transform.position = transform.position;
+        //Destroy(collision.gameObject);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == this.enemyObjectTag){
+            this.tomeChichi(collision);
+        }
     }
 
     // Update is called once per frame
