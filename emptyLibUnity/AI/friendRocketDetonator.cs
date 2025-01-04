e338c387f4bcea6bc74391d9b62b57aaa8128fcd
rgarro <rgarro@gmail.com>
2025-01-04 14:25:03 -0600

    naish hoover cross over blue train and coltrane

--- emptyLibUnity/AI/friendRocketDetonator.cs
+++ emptyLibUnity/AI/friendRocketDetonator.cs
@@ -1,7 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+ *             +
+ *            /_\
+ *  ,%%%______|O|
+ *  %%%/_________\
+ *  `%%| /\[][][]|%
+ * ___||_||______|%&,__ hjw
+ *henry es playo, quincho es gay, mamerto es gay , bolincha es gay, ya no tienen gracia y los caga el espiritu santo 
+ *
+ * will detect round collitions on player and increase player damage
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class friendRocketDetonator : MonoBehaviour
 {
     // Start is called before the first frame update
