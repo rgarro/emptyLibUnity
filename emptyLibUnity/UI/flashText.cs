54a8e332105c857fab071a9521e44ff8ccc29d53
rgarro <rgarro@gmail.com>
2025-11-08 00:04:21 -0600

    adding components

---
+++ emptyLibUnity/UI/flashText.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *                       ---                                     
+ *                    -        --                             
+ *                --( /     \ )XXXXXXXXXXXXX                   
+ *            --XXX(   O   O  )XXXXXXXXXXXXXXX-              
+ *           /XXX(       U     )        XXXXXXX\               
+ *         /XXXXX(              )--   XXXXXXXXXXX\             
+ *        /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+ *        XXXXX/   /            XXXXXX   \   \XXXXX----        
+ *        XXXXXX  /          XXXXXX         \  ----  -         
+ * --     XXX  /          XXXXXX      \           ---        
+ *  --  --  /      /\  XXXXXX            /     ---=         
+ *    -        /    XXXXXX              '--- XXXXXX         
+ *      --\/XXX\ XXXXXX                      /XXXXX         
+ *        \XXXXXXXXX                        /XXXXX/
+ *         \XXXXXX                         /XXXXX/         
+ *           \XXXXX--  /                -- XXXX/       
+ *            --XXXXXXX---------------  XXXXX--         
+ *               \XXXXXXXXXXXXXXXXXXXXXXXX-            
+ *                 --XXXXXXXXXXXXXXXXXX-                      
+ * Will render flashMsg string displaying configured style at given rect position
+ *
+ *
+ *
+ *@author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
+ */
+public class flashText : MonoBehaviour
+{
+    public float windowX = 10;
+    public float windowY = 65;
+    public float windowHeight = 170;
+    public float windowWidth = 150;
+	private Rect windowRect;
+    
+    public GUIStyle style;
+    
+    public string flashMsg = "I aint afraid of no ghost..";
+    private AudioSource servoSoundPlayer;
+	public AudioClip flashSoundClip;
+
+    public bool playSoundOnUpdate = true;
+    public float soundVolume = 0.7F;
+    public float beforeNextMsgWait = 2.2F;
+    
+    void Start()
+    {
+        this.servoSoundPlayer = GetComponent<AudioSource>();
+    }
+
+    private void playServoSoundOn(){
+        this.servoSoundPlayer.PlayOneShot(this.flashSoundClip, this.soundVolume);
+    }
+
+    void OnGUI(){
+		this.windowRect = new Rect(this.windowX,this.windowY,this.windowHeight,this.windowWidth);		
+		GUI.Label(this.windowRect,this.flashMsg,this.style);
+	}
+
+    public void setFlashMsg(string msg){
+        //Debug.Log("msg.." + msg);
+        this.flashMsg = msg;
+        if(this.playSoundOnUpdate){
+            this.playServoSoundOn();
+            //this.servoSoundPlayer.Stop ();
+        }
+        //yield return new WaitForSeconds (this.beforeNextMsgWait);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ emptyLibUnity/UI/restartTimerOut.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *            _________________________
+ *          ,'        _____            `.
+ *        ,'       _.'_____`._           `.
+ *       :       .'.-'  12 `-.`.           \
+ *       |      /,' 11  .   1 `.\           :
+ *       ;     // 10    |     2 \\          |
+ *     ,'     ::        |        ::         |
+ *   , '       || 9   ---O      3 ||         |
+ *  /         ::                 ;;         |
+ * :           \\ 8           4 //          |
+ * |            \`. 7       5 ,'/           |
+ * |             '.`-.__6__.-'.'            |
+ * :              ((-._____.-))             ;
+ *  \             _))       ((_            /
+ *   `.          '--'       '--'         ,'
+ *     `.______________________________,'
+ *         ,-.
+ *         `-'
+ *            O
+ *             o
+ *              .     ____________
+ *             ,('`)./____________`-.-,|
+ *            |'-----\\--------------| |
+ *            |_______^______________|,|
+ *            |                      |   SSt
+ * Displays cancellable TimeOut until restarts the entire game
+ * supposed to be Relayed remotely by player action
+ *
+ * Un Delegado es un Relay Remoto Accesible publicamente, decia un perverso que compro un titulo en la U latina y se posaba de dueno ...
+ *
+ *
+ *
+ *@author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
+ */
+public class restartTimerOut : MonoBehaviour
+{
+    public float windowX = 10;
+    public float windowY = 65;
+    public float windowHeight = 170;
+    public float windowWidth = 150;
+	private Rect windowRect;
+    
+    public GUIStyle style;
+    
+    private string timeOutStr;
+    private AudioSource soundPlayer;
+    public AudioClip timerSoundClip;
+
+    public bool playSoundOnTimerOn = true;
+    public float soundVolume = 0.7F;
+    public int SecondsFromTimeOut = 180;//small time shot away ...
+    public string concatLegend = "seconds remaining";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.soundPlayer = GetComponent<AudioSource>();
+    }
+
+    private void playSoundOn(){
+        this.soundPlayer.PlayOneShot(this.timerSoundClip, this.soundVolume);
+    }
+
+     void OnGUI(){
+		this.windowRect = new Rect(this.windowX,this.windowY,this.windowHeight,this.windowWidth);		
+		GUI.Label(this.windowRect,this.timeOutStr,this.style);
+	}
+
+    public void startTimer(){
+        StartCoroutine(updateTimerString());
+    }
+
+    public void stopTimer(){
+        StopCoroutine(updateTimerString());
+    }
+
+    IEnumerator updateTimerString(){
+        int seconds = this.SecondsFromTimeOut;
+		while(true){
+            seconds = seconds - 1;
+            this.timeOutStr = seconds + " " + this.concatLegend;
+			Debug.Log("timeStr: "+this.timeOutStr);
+            if(seconds > 2){
+                //restart game here
+            }
+			yield return new WaitForSeconds (1);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
